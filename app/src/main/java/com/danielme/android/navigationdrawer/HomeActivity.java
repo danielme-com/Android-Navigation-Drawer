250aa425a81b7cf3c22c669e9579a555deab0f86
dani <danielme_com@yahoo.com>
2020-11-18 19:26:39 +0100

    animación para el cambio de fragment

--- app/src/main/java/com/danielme/android/navigationdrawer/HomeActivity.java
+++ app/src/main/java/com/danielme/android/navigationdrawer/HomeActivity.java
@@ -1,18 +1,19 @@
 package com.danielme.android.navigationdrawer;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import com.google.android.material.navigation.NavigationView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -87,8 +88,11 @@ public class HomeActivity extends AppCompatActivity
     }
 
     Fragment fragment = HomeContentFragment.newInstance(getString(title));
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    fragmentManager.beginTransaction().replace(R.id.home_content, fragment).commit();
+    getSupportFragmentManager()
+            .beginTransaction()
+            .setCustomAnimations(R.anim.bottom_nav_enter, R.anim.bottom_nav_exit)
+            .replace(R.id.home_content, fragment)
+            .commit();
 
     setTitle(getString(title));
 
--- app/src/main/java/com/danielme/android/navigationdrawer/HomeContentFragment.java
+++ app/src/main/java/com/danielme/android/navigationdrawer/HomeContentFragment.java
@@ -2,18 +2,15 @@ package com.danielme.android.navigationdrawer;
 
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * Created by dani on 28/12/16.
- */
-public class HomeContentFragment extends Fragment {
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
+public class HomeContentFragment extends Fragment {
 
   private static final String TEXT = "text";
 
